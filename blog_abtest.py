bc5c89caecb4e7217be0001b3aa27986fb918eeb
Dragon <ruuddyd@gmail.com>
2021-01-13 21:57:33 +0900

    setup

--- blog_abtest.py
+++ blog_abtest.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template, make_response
-from flask_login import login_manager, current_user, login_required, login_user, logout_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
+from blog_view import blog
 import os
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -9,7 +10,9 @@ app = Flask(__name__, static_url_path='/static')
 CORS(app)
 app.secure_key = 'static'
 
-login_manager = login_manager()
+
+app.register_blueprint(blog.blog_abtest, url_prefix='/blog')
+login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = 'strong'
 
@@ -18,7 +21,7 @@ def load_user(user_id):
     return User.get(user_id)
 
 
-@login_manager.user_unauthorized_handler
+@login_manager.unauthorized_handler
 def unauthorized():
     return make_response(jsonify(success=False), 401)
 
